8243b9dca7dc9915698b05b317a5c27013a62000
Korytin Nikita <korytin.nikita@gmail.com>
2026-06-18 20:16:32 +0500

    Debug converter adding

---
+++ src/Revit.Linter.DiagnosticReportPresenter/Infrasructure/Converters/DebugConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Revit.Linter.DiagnosticReportPresenter.Infrasructure.Converters;
+
+public class DebugConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        System.Diagnostics.Debug.WriteLine($"=== DEBUG CONVERTER ===");
+        System.Diagnostics.Debug.WriteLine($"Value: {value}");
+        System.Diagnostics.Debug.WriteLine($"Value Type: {value?.GetType()}");
+        System.Diagnostics.Debug.WriteLine($"Parameter: {parameter}");
+
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
