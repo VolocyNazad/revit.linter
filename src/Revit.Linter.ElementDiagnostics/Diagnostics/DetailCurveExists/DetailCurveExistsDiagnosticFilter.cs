be8cabc284f0b737c36f3814d5f8838f7599e394
volocy_nazad <korytin.nikita@mail.ru>
2026-07-22 12:25:48 +0500

    fix: diagnostic bugs

--- src/Revit.Linter.ElementDiagnostics/Diagnostics/DetailCurveExists/DetailCurveExistsDiagnosticFilter.cs
+++ src/Revit.Linter.ElementDiagnostics/Diagnostics/DetailCurveExists/DetailCurveExistsDiagnosticFilter.cs
@@ -1,9 +1,12 @@
-﻿namespace Revit.Linter.ElementDiagnostics.Diagnostics.DetailCurveExists;
+﻿using Toolkit.Revit.Extensions;
+
+namespace Revit.Linter.ElementDiagnostics.Diagnostics.DetailCurveExists;
 
 internal sealed class DetailCurveExistsDiagnosticFilter : IElementDiagnosticFilter
 {
     public ElementDiagnosticId Identity => ElementDiagnosticIdCollector.DetailCurveExists;
 
     public bool IsRelevantFor(Document document, Element element)
-        => element is DetailCurve;
+        => element is DetailCurve && element.Category != null
+        && element.Category.BuiltInCategory == BuiltInCategory.OST_Lines;
 }
--- src/Revit.Linter.ElementDiagnostics/Diagnostics/ModelCurveExists/ModelCurveExistsDiagnosticFilter.cs
+++ src/Revit.Linter.ElementDiagnostics/Diagnostics/ModelCurveExists/ModelCurveExistsDiagnosticFilter.cs
@@ -1,9 +1,12 @@
-﻿namespace Revit.Linter.ElementDiagnostics.Diagnostics.ModelCurveExists;
+﻿using Toolkit.Revit.Extensions;
+
+namespace Revit.Linter.ElementDiagnostics.Diagnostics.ModelCurveExists;
 
 internal sealed class ModelCurveExistsDiagnosticFilter : IElementDiagnosticFilter
 {
     public ElementDiagnosticId Identity => ElementDiagnosticIdCollector.ModelCurveExists;
 
     public bool IsRelevantFor(Document document, Element element)
-        => element is ModelCurve;
+        => element is ModelCurve && element.Category != null 
+        && element.Category.BuiltInCategory == BuiltInCategory.OST_Lines;
 }
